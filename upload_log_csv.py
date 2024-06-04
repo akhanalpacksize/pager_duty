85cea32b0506ced35df9de96487dcb91fc2c80a0
akhanalpacksize <ayush.khanal@packsize.com>
2024-06-04 09:50:04 +0545

    chnaged dataset name

--- upload_log_csv.py
+++ upload_log_csv.py
@@ -74,7 +74,7 @@ def generate_update_schema():
 
     updated_schema_column = generate_schema_columns(headers)
 
-    dataset_name = "DataSet Summary"
+    dataset_name = "Pager Duty"
     try:
         # UPDATE DATASET FOR DOMO
         update_schema(dataset_name, updated_schema_column, access_token)
